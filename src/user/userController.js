cd902f4493f0425cefbc4edd35314313948c3316
Rachel Groves <grovrac09@gmail.com>
2021-12-14 13:33:14 +0000

    functional update

--- src/user/userController.js
+++ src/user/userController.js
@@ -6,26 +6,28 @@ exports.addUser = async (req, res) => {
         await newUser.save();
         res.status(200).send({message: "Successfully added user.", newUser});
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        res.status(500).send({message: "Unsuccessful, please try again."});
     }
 };
 
 exports.listUser = async (req, res) => {
     try {
         res.status(200).send(await User.find({}));
-    }catch (error) {
+    } catch (error) {
         console.log(error)
     }
 }
 
-// exports.updateUser = async (req, res) => {
-//     try {
-//         await User.findByIdAndUpdate(req, res);
-//         console.log(`Successfully updated user.`)
-//     } catch (error) {
-//         console.log(error)
-//     } 
-// };
+exports.updateUser = async (req, res) => {
+    try {
+        updateFunc = await User.findByIdAndUpdate(req.body._id, req.body);
+        const updatedUser = await User.findById(req.body._id);
+        res.status(200).send({message: "Successfully updated user.", updatedUser});
+    } catch (error) {
+        console.log(error)
+    } 
+};
 
 // exports.deleteMusic = async () => {
 //     try {
--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,10 +1,11 @@
 const {Router} = require("express");
 const {addUser, listUser, updateUser, deleteUser} = require("./userController");
+const {hashPassword} = require("../middleware");
 const userRouter = Router();
 
-userRouter.post("/user", addUser);
+userRouter.post("/user", hashPassword, addUser);
 userRouter.get("/user", listUser);
-// userRouter.put("/user", updateUser);
+userRouter.put("/user", updateUser);
 // userRouter.delete("/user", deleteUser);
 
 module.exports = userRouter;
